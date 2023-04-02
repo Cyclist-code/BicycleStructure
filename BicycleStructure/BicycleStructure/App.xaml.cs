182b1cd89cabc56fec1791a56142291694b79e1c
Gostev Konstantin <KOSTYA190900@yandex.ru>
2023-04-02 11:29:48 +0700

    Update App.xaml.cs

--- BicycleStructure/BicycleStructure/App.xaml.cs
+++ BicycleStructure/BicycleStructure/App.xaml.cs
@@ -10,7 +10,7 @@ namespace BicycleStructure
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new AppShell();
         }
 
         protected override void OnStart()

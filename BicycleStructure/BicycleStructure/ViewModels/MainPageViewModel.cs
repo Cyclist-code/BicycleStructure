101a2b18ed9a7501d0d379980b4747819f6ea177
Gostev Konstantin <KOSTYA190900@yandex.ru>
2023-04-03 16:41:14 +0700

    Adding logic to open AboutPage

--- BicycleStructure/BicycleStructure/ViewModels/MainPageViewModel.cs
+++ BicycleStructure/BicycleStructure/ViewModels/MainPageViewModel.cs
@@ -2,6 +2,7 @@
 using System.Windows.Input;
 using Xamarin.Essentials;
 using Xamarin.Forms;
+using BicycleStructure.Views;
 
 namespace BicycleStructure.ViewModels
 {
@@ -10,6 +11,18 @@ namespace BicycleStructure.ViewModels
         public MainPageViewModel()
         {
             Title = "Bicycle Structure";
+            OpenAboutPageCommand = new Command(OpenPage);
         }
+
+        #region Commands
+        public ICommand OpenAboutPageCommand { get; }
+        #endregion
+
+        #region Methods
+        private void OpenPage()
+        {
+            Application.Current.MainPage.Navigation.PushAsync(new AboutPage());
+        }
+        #endregion
     }
 }

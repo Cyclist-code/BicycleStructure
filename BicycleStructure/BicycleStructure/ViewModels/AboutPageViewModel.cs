a345543562c501a0c5ef37b8a9e416dbcc55fa86
Gostev Konstantin <KOSTYA190900@yandex.ru>
2023-04-04 09:11:22 +0700

    Create AboutPageViewModel.cs

---
+++ BicycleStructure/BicycleStructure/ViewModels/AboutPageViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BicycleStructure.ViewModels
+{
+    public class AboutPageViewModel : BaseViewModel
+    {
+        public AboutPageViewModel()
+        {
+            
+        }
+    }
+}

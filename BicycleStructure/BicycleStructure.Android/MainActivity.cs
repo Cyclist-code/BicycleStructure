9ba88383178284715a71e2cd40f5e57029409007
Gostev Konstantin <KOSTYA190900@yandex.ru>
2023-04-04 08:42:52 +0700

    Adding an app icon

--- BicycleStructure/BicycleStructure.Android/MainActivity.cs
+++ BicycleStructure/BicycleStructure.Android/MainActivity.cs
@@ -7,7 +7,7 @@ using Android.OS;
 
 namespace BicycleStructure.Droid
 {
-    [Activity(Label = "BicycleStructure", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "Bicycle Structure", Icon = "@mipmap/app_icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)

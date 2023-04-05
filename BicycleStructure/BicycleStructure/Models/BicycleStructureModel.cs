14c954da3776d7810f2438dc7a3c30df4c99d76c
Gostev Konstantin <KOSTYA190900@yandex.ru>
2023-04-05 17:33:59 +0700

    Create BicycleStructureModel.cs

---
+++ BicycleStructure/BicycleStructure/Models/BicycleStructureModel.cs
@@ -0,0 +1,11 @@
+﻿using Xamarin.Forms;
+
+namespace BicycleStructure.Models
+{
+    public class BicycleStructureModel
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public ImageSource ImagePath { get; set; }
+    }
+}
